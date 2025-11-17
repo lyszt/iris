0541d88e6d32f701061bc4f8e1951f0908432e4f
lyszt <kalliddel@gmail.com>
2025-11-17 11:55:48 -0300

    first steps in server-side

--- prostagma_server/lib/prostagma_server_web/router.ex
+++ prostagma_server/lib/prostagma_server_web/router.ex
@@ -2,28 +2,29 @@ defmodule ProstagmaServerWeb.Router do
   use ProstagmaServerWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {ProstagmaServerWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {ProstagmaServerWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", ProstagmaServerWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :home
+    get("/", PageController, :home)
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ProstagmaServerWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", ProstagmaServerWeb do
+    pipe_through(:api)
+
+    get("/projects", ProjectController, :index)
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:prostagma_server, :dev_routes) do
@@ -35,10 +36,10 @@ defmodule ProstagmaServerWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: ProstagmaServerWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: ProstagmaServerWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
